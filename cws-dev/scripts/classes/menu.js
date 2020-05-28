54f1550795593444393fa9420501ffe85af11af8
Greg Rowles <greg.rowles@gmail.com>
2020-05-28 16:30:56 +0200

    work-in-progress

--- cws-dev/scripts/classes/menu.js
+++ cws-dev/scripts/classes/menu.js
@@ -4,12 +4,12 @@
 function Menu() {}
 
 Menu.menuItems_Loaded = []; //varInfo = 'eRefWSDev3'; //'eRefWSDev3';	eRefWSStage		// Need to be dynamically retrieved
-Menu.menuJson_Statistics = { id: "statisticsPage", name: "Statistics", term: "", icon: "statistics", group: true, groupHeader: false };
-Menu.menuJson_Settings = { id: "settingsPage", name: "Settings", term: "", icon: "settings", group: false, groupHeader: false };
-Menu.menuJson_HNQIS = { id: "HNQIS", name: "HNQIS", term: "", icon: "settings", group: false, groupHeader: false };
-Menu.menuJson_MyDetails = { id: "myDetails", name: "My Details", term: "", icon: "settings", group: false, groupHeader: false };
-Menu.menuJson_About = { id: "aboutPage", name: "About", term: "", icon: "about", group: true, groupHeader: false };
-Menu.menuJson_LogOut = { id: "logOut", name: "Log out", term: "menu_logout", icon: "logout", group: true, groupHeader: false };
+Menu.menuJson_Statistics = { id: "statisticsPage", name: "Statistics", term: "", icon: "statistics", groupBefore: true, groupAfter: false };
+Menu.menuJson_Settings = { id: "settingsPage", name: "Settings", term: "", icon: "settings", groupBefore: false, groupAfter: false };
+Menu.menuJson_HNQIS = { id: "HNQIS", name: "HNQIS", term: "", icon: "settings", groupBefore: false, groupAfter: false };
+Menu.menuJson_MyDetails = { id: "myDetails", name: "My Details", term: "", icon: "settings", groupBefore: false, groupAfter: false };
+Menu.menuJson_About = { id: "aboutPage", name: "About", term: "", icon: "about", groupBefore: true, groupAfter: true };
+Menu.menuJson_LogOut = { id: "logOut", name: "Log out", term: "menu_logout", icon: "logout", groupBefore: false, groupAfter: true };
 
 // ==== Methods ======================
 
