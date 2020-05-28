ac3c676ababbab8f72fba6981214df8f45740daa
James Chang <jamesbchang@gmail.com>
2020-05-29 00:34:33 +0900

    sync status related logic update

--- cws-dev/scripts/constants/constants.js
+++ cws-dev/scripts/constants/constants.js
@@ -6,12 +6,17 @@ Constants.storageName_activityList = "activityList";    // Should add with userN
 Constants.storageName_clientList = "clientList";
 Constants.storageName_session = "session";
 
+// ONGOING STATUS ONES:
+Constants.status_queued = "queued"; // initialize from dcd@XX ?
+Constants.status_failed = "failed"; // submit failed case..
+Constants.status_hold = "paused"; // initialize from dcd@XX ? //REMOVE? We don't "PAUSE" or HOLD records (yet)
+Constants.status_processing = "processing"; // initialize from dcd@XX ? //REMOVE? We don't "PAUSE" or HOLD records (yet)
+// FINISHED STATUS ONES:
+Constants.status_downloaded = "downloaded"; // DO NOT NEED THIS - SAME AS 'synced'?
 Constants.status_submit = "submit"; // 'synced' <-- CHANGE!!! WITH CONFIG CHANGE
 Constants.status_submit_wMsg = "submit_wMsg"; 
-Constants.status_queued = "queued"; // initialize from dcd@XX ?
-Constants.status_failed = "failed"; // initialize from dcd@XX ?
-Constants.status_hold = "paused"; // initialize from dcd@XX ? //REMOVE? We don't "PAUSE" or HOLD records (yet)
-Constants.status_downloaded = "downloaded"; // DO NOT NEED THIS - SAME AS 'synced'?
+Constants.status_error = "error"; // 
+
 
 //Constants.fixedActiveUserId = "qwertyuio3"; // Replaced with SessionManager.sessionData.login_UserName
 
