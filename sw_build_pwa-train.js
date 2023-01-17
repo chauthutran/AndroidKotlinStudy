a77d3ab1154334e928b37daaec2bbf6b8ff04786
James Chang <jamesbchang@gmail.com>
2023-01-17 17:21:21 +0900

    update for pwa-train

---
+++ sw_build_pwa-train.js
@@ -0,0 +1,44 @@
+
+// Require WorkBox build
+const {generateSW} = require('workbox-build');
+//const {Workbox} = require('workbox-window');
+
+generateSW({
+  mode: 'production',
+  swDest: 'pwa-train/service-worker.js',
+  globDirectory: 'pwa-train',
+  globPatterns: [
+    '**/*.{html,css,js,gif,jpg,png,svg,mp3,wav,json}'
+  ],
+  skipWaiting: true,
+  clientsClaim: true,
+  offlineGoogleAnalytics: false,
+  cleanupOutdatedCaches: false,
+  importScripts: [ './swExtra.js' ],
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com.*/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'googleFonts'
+      }
+    },
+    {
+      urlPattern: /^https:\/\/matomo\.psi-mis\.org\/matomo\.js/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'matomo'
+      }
+    },
+    {
+      urlPattern: /\/jobs\//,
+      handler: 'CacheOnly',
+      options: {
+        cacheName: 'jobTest2'
+      }
+    }
+  ]
+
+}).then(({count, size}) => {
+  console.log(`Generated new service worker with ${count} precached files, totaling ${size} bytes.`);
+}).catch(console.error);
