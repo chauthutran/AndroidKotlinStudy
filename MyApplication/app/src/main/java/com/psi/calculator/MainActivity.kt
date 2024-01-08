31391f4f4e892229870c9118994dd194351d846e
chauthutran <cthutran@gmail.com>
2024-01-08 22:09:59 +0900

    Updated

--- MyApplication/app/src/main/java/com/psi/calculator/MainActivity.kt
+++ MyApplication/app/src/main/java/com/psi/calculator/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.psi.calculator
 
-import android.R
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
-
+import android.R
 
 class MainActivity : AppCompatActivity() {
+    private var txtValue: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list_item activity_main)
 
-//        mButton = findViewById(R.id.);
-//        mButton.setOnClickListener(this);
+        txtValue = findViewById(R.id.txtValue)
     }
 
-    fun onClick(view: View) {
-        when (view.id) {
-            R.id.button_send -> {}
+    fun onClickHandler(view: View) {
+        var formular: String = txtValue?.text.toString()
+        var btnObj: Button = this.findViewById<Button>(view.id)
+        var clickedValue = btnObj.text
+        if ( btnObj.id !=  R.id.btnEquals ) {
+            if( view.id != R.id.btnDecimal && view.id != R.id.btnSign ) {
+                txtValue!!.text = formular + " " + clickedValue
+            }
+            else
+            {
+                txtValue!!.text = formular + clickedValue
+            }
         }
     }
 }
